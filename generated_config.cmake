b2f1c67fa517f2c0852f71a76384b26ce00533c4
kaimarks9 <123796253+kaimarks9@users.noreply.github.com>
2023-02-04 10:59:03 +0700

    Add files via upload

---
+++ generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/marks/Android/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/marks/AndroidStudioProjects/music_box" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.15.6+37" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 15 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 6 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 37 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/marks/Android/flutter"
+  "PROJECT_DIR=/home/marks/AndroidStudioProjects/music_box"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/marks/AndroidStudioProjects/music_box/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/marks/AndroidStudioProjects/music_box/lib/main.dart"
+)
